cc52460727de5198ec9f8b26a66e059551a2e683
Raivydas Šimėnas <raivydas.simenas@icloud.com>
2025-09-25 13:58:41 +0300

    Selve S03

--- MIL copy/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
+++ MIL copy/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
@@ -113,7 +113,10 @@ example : 0 ≤ a ^ 2 := by
   -- exact sq_nonneg a
 
 example (h : a ≤ b) : c - exp b ≤ c - exp a := by
-  sorry
+  apply add_le_add_left
+  apply neg_le_neg
+  apply exp_le_exp.mpr
+  exact h
 
 example : 2*a*b ≤ a^2 + b^2 := by
   have h : 0 ≤ a^2 - 2*a*b + b^2
@@ -133,7 +136,29 @@ example : 2*a*b ≤ a^2 + b^2 := by
     _ ≥ 0 := by apply pow_two_nonneg
   linarith
 
+theorem fact1 : a*b*2 ≤ a^2 + b^2 := by
+  have h : 0 ≤ a^2 - 2*a*b + b^2
+  calc
+    a^2 - 2*a*b + b^2 = (a - b)^2 := by ring
+    _ ≥ 0 := by apply pow_two_nonneg
+  linarith
+
+theorem fact2 : -(a*b)*2 ≤ a^2 + b^2 := by
+  have h : 0 ≤ a^2 + 2*a*b + b^2
+  calc
+    a^2 + 2*a*b + b^2 = (a + b)^2 := by ring
+    _ ≥ 0 := by apply pow_two_nonneg
+  linarith
+
 example : |a*b| ≤ (a^2 + b^2)/2 := by
-  sorry
+  have h : (0 : ℝ) < 2 := by norm_num
+  apply abs_le'.mpr
+  constructor
+  · rw [le_div_iff₀ h]
+    apply fact1
+  rw [le_div_iff₀ h]
+  apply fact2
+
+
 
 #check abs_le'.mpr
